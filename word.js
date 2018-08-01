a86fef20c1f5f7734a0f7fbf3e3ac751b92c84a7
sadakosa <sabrinachow98@gmail.com>
2018-08-01 10:02:16 +0800

    done yay

--- word.js
+++ word.js
@@ -1,6 +1,6 @@
 let word = process.argv[2];
 
-console.log(word);
+console.log('word: ' + word);
 
 let vowels = {
     'a': 1,
@@ -10,19 +10,83 @@ let vowels = {
     'u': 1
 }
 
-if (vowels[word.split('')[0]] != null) { //it is a vowel
-    word = word + 'way';
-} else { //it is a consonant
-    tempArr = word.split('');
+function normal () {
+    if (vowels[word.split('')[0]] != null) { //it is a vowel
+        word = word + 'way';
+    } else { //it is a consonant
+        tempArr = word.split('');
+    
+        let count = 0;
+        while (vowels[tempArr[0]] == null && count !== tempArr.length) {
+            tempArr.push(tempArr[0]);
+            tempArr.shift();
+            count ++;
+        }
+    
+        word = tempArr.join('') + 'ay';
+    }
+    
+    console.log(word);
+} 
 
-    let count = 0;
-    while (vowels[tempArr[0]] == null && count !== tempArr.length) {
+
+function bonuses () {
+    if (vowels[word.split('')[0]] != null) { //it is a vowel
+        word = word + 'way';
+    } else if (word.split('')[0] + word.split('')[1] == 'qu') { //word starts with 'qu'
+        tempArr = word.split('');
         tempArr.push(tempArr[0]);
+        tempArr.push(tempArr[1]);
         tempArr.shift();
-        count ++;
+        tempArr.shift();
+        
+        let count = 2;
+        let check = tempArr[0] == 'y';
+        while (check || vowels[tempArr[0]] == null && count !== tempArr.length ) {
+            tempArr.push(tempArr[0]);
+            tempArr.shift();
+            count ++;
+            check = tempArr[0] == 'y';
+        }
+
+        word = tempArr.join('') + 'ay';
+    } else if (word.split('')[0] + word.split('')[1] + word.split('')[2] == 'squ') {//word starts with 'qu' or 'squ'
+    
+        tempArr = word.split('');
+        tempArr.push(tempArr[0]);
+        tempArr.push(tempArr[1]);
+        tempArr.push(tempArr[2]);
+        tempArr.shift();
+        tempArr.shift();
+        tempArr.shift();
+        
+        let count = 3;
+        let check = tempArr[0] == 'y';
+        while (check || vowels[tempArr[0]] == null && count !== tempArr.length) {
+            tempArr.push(tempArr[0]);
+            tempArr.shift();
+            count ++;
+            count = tempArr[0] == 'y';
+        }
+
+        word = tempArr.join('') + 'ay';
+
+    }else { //it is a consonant
+        tempArr = word.split('');
+    
+        let count = 0;
+        let check = true;
+        while (check || vowels[tempArr[0]] == null && count !== tempArr.length) {
+            tempArr.push(tempArr[0]);
+            tempArr.shift();
+            count ++;
+            check = tempArr[0] == 'y';
+        }
+    
+        word = tempArr.join('') + 'ay';
     }
 
-    word = tempArr.join('') + 'ay';
+    console.log(word)
 }
 
-console.log(word);
+bonuses();
